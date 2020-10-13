1ae850357891256116d1483a61d93c26a6738bbb
Prasanta Kakati <prasantakakati1994@gmail.com>
2020-10-13 14:07:30 +0530

    Proper pool stats API

--- src/api/poolStat.js
+++ src/api/poolStat.js
@@ -1,5 +1,10 @@
 import { jsonAPI } from 'api/base'
 
+export const getPools = () => {
+    const url = `/poolstats/pools/`
+    return jsonAPI(api => api.get(url))
+}
+
 export const getPoolStat = url => {
     return jsonAPI(api => api.get(url))
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,94 +2,165 @@ import React from 'react'
 import classnames from 'classnames'
 import isEmpty from 'lodash/isEmpty'
 
-import { getPoolStat } from 'api/poolStat'
+import { getPoolStat, getPools } from 'api/poolStat'
 
-import bazaLogo from 'assets/images/baza_logo.svg'
 import s from './Home.module.scss'
 
 class Home extends React.Component {
     state = {
+        pools: [],
         poolsStat: {},
     }
 
     componentDidMount() {
-        getPoolStat('https://pool.baza.foundation/api/v2/stats').then(
-            response =>
-                this.setState({
-                    poolsStat: { baza: response.data },
-                })
+        getPools().then(response =>
+            this.setState({ pools: response.data }, () => {
+                this.setPoolsStat()
+                this.setPoolStatInterval = setInterval(
+                    () => this.setPoolsStat(),
+                    10000
+                )
+            })
+        )
+    }
+
+    componentWillUnmount() {
+        if (this.setPoolStatInterval) {
+            clearInterval(this.setPoolStatInterval)
+        }
+    }
+
+    setPoolsStat = () => {
+        const { pools } = this.state
+        for (const pool of pools) {
+            getPoolStat(pool.pool_stats_api_url).then(response =>
+                this.setState(prevState => ({
+                    poolsStat: {
+                        ...prevState.poolsStat,
+                        [pool.symbol]: response.data,
+                    },
+                }))
+            )
+        }
+    }
+
+    getReadableHashRateString = hashrate => {
+        let i = 0
+        const byteUnits = [' H', ' KH', ' MH', ' GH', ' TH', ' PH']
+        while (hashrate > 1000) {
+            hashrate = hashrate / 1000
+            i++
+        }
+        return hashrate.toFixed(2) + byteUnits[i]
+    }
+
+    getPoolPercentage = poolStat => {
+        const netWorkHash =
+            poolStat.network.difficulty / poolStat.config.coinDifficultyTarget
+        const poolHash = poolStat.pool.hashrate
+        return Number((poolHash / netWorkHash) * 100).toFixed(2)
+    }
+
+    renderOnePoolStat = (pool, i) => {
+        const poolStat = this.state.poolsStat[pool.symbol]
+        return (
+            <div className="pool" key={i}>
+                <div className="pool-header">
+                    {pool.logo_url && (
+                        <img
+                            className="img-fluid mr-1"
+                            src={pool.logo_url}
+                            alt="Token Logo"
+                        />
+                    )}
+                    <h6>{pool.symbol}</h6>
+                    <div className="flex-1" />
+                    <span>{pool.last_price} USDT</span>
+                </div>
+                <div className="pool-info">
+                    {!isEmpty(poolStat) && (
+                        <a
+                            href={pool.pool_url}
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            <div className="d-flex mb-2">
+                                <b>
+                                    <i className="fas fa-tachometer-alt" />{' '}
+                                    Network hashrate:{' '}
+                                </b>
+                                <span className="flex-1" />
+                                <span>
+                                    {this.getReadableHashRateString(
+                                        poolStat.network.difficulty /
+                                            poolStat.config.coinDifficultyTarget
+                                    )}
+                                    /s
+                                </span>
+                            </div>
+                            <div className="d-flex mb-2">
+                                <b>
+                                    <i className="fas fa-tachometer-alt" /> Pool
+                                    hashrate:{' '}
+                                </b>
+                                <span className="flex-1" />
+                                <span>
+                                    {this.getReadableHashRateString(
+                                        poolStat.pool.hashrate
+                                    )}
+                                    /s
+                                </span>
+                            </div>
+                            <div className="d-flex mb-2">
+                                <b>
+                                    <i className="fas fa-chart-pie" /> Pool
+                                    percentage:{' '}
+                                </b>
+                                <span className="flex-1" />
+                                <span>
+                                    {this.getPoolPercentage(poolStat)} %
+                                </span>
+                            </div>
+                            <div className="d-flex mb-2">
+                                <b>
+                                    <i className="fas fa-users" /> Miners:{' '}
+                                </b>
+                                <span className="flex-1" />
+                                <span>{poolStat.pool.miners}</span>
+                            </div>
+                            <div className="d-flex mb-2">
+                                <b>
+                                    <i className="fas fa-money-bill-alt" /> Fee:{' '}
+                                </b>
+                                <span className="flex-1" />
+                                <span>{poolStat.config.fee} %</span>
+                            </div>
+                            <div className="d-flex mb-2">
+                                <b>
+                                    <i className="fas fa-certificate" /> Block
+                                    Reward:{' '}
+                                </b>
+                                <span className="flex-1" />
+                                <span>
+                                    {Number(
+                                        poolStat.network.reward / 1000000
+                                    ).toFixed(6)}
+                                </span>
+                            </div>
+                        </a>
+                    )}
+                </div>
+            </div>
         )
     }
 
     render() {
         const { className } = this.props
-        const { poolsStat } = this.state
+        const { pools } = this.state
         const cx = classnames(s.container, className)
 
         return (
             <div className={cx}>
-                <div className="pools">
-                    <div className="pool">
-                        <div className="pool-header">
-                            <img
-                                className="img-fluid mr-1"
-                                src={bazaLogo}
-                                alt="Baza Logo"
-                            />
-                            <h6>BAZA(BAZ)</h6>
-                        </div>
-                        <div className="pool-info">
-                            {!isEmpty(poolsStat.baza) && (
-                                <a
-                                    href="https://pool.baza.foundation"
-                                    target="_blank"
-                                    rel="noopener noreferrer">
-                                    <div className="d-flex mb-2">
-                                        <b>
-                                            <i className="fas fa-tachometer-alt" />{' '}
-                                            Pool hashrate:{' '}
-                                        </b>
-                                        <span className="flex-1" />
-                                        <span>
-                                            {poolsStat.baza.pool.hashrate} H/s
-                                        </span>
-                                    </div>
-                                    <div className="d-flex mb-2">
-                                        <b>
-                                            <i className="fas fa-users" />{' '}
-                                            Miners:{' '}
-                                        </b>
-                                        <span className="flex-1" />
-                                        <span>
-                                            {poolsStat.baza.pool.miners}
-                                        </span>
-                                    </div>
-                                    <div className="d-flex mb-2">
-                                        <b>
-                                            <i className="fas fa-money-bill-alt" />{' '}
-                                            Fee:{' '}
-                                        </b>
-                                        <span className="flex-1" />
-                                        <span>
-                                            {poolsStat.baza.config.fee} %
-                                        </span>
-                                    </div>
-                                    <div className="d-flex mb-2">
-                                        <b>
-                                            <i className="fas fa-certificate" />{' '}
-                                            Block Reward:{' '}
-                                        </b>
-                                        <span className="flex-1" />
-                                        <span>
-                                            {poolsStat.baza.network.reward /
-                                                1000000}
-                                        </span>
-                                    </div>
-                                </a>
-                            )}
-                        </div>
-                    </div>
-                </div>
+                <div className="pools">{pools.map(this.renderOnePoolStat)}</div>
             </div>
         )
     }
